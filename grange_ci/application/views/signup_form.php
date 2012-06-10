308f996a3fb5852b4eee8d0ca7bdda6a53f7ef05
Brian Tria <btria959@gmail.com>
2012-06-10 14:21:27 +0800

    new file:   grange_ci/application/views/signup_form.php 	deleted:    grange_ci/application/views/signup.php

---
+++ grange_ci/application/views/signup_form.php
@@ -0,0 +1,14 @@
+<div class='main_content'>
+	<div id='notes'>
+  	* indicates required fields.
+  </div><!-- end #notes -->
+<?php
+	$attr = array(
+			'id' => 'signup_form',
+			'name' => 'signup_form'
+		);
+	echo form_open('display/signup', $attr);
+		
+	echo form_close();
+?>
+</div><!-- end .main_content -->
