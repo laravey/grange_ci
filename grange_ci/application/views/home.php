db4fbd3e3e560583e09f3b75c8015e5fad1630ec
Brian Tria <btria959@gmail.com>
2012-06-17 15:38:19 +0800

    modified:   grange_ci/application/views/home.php 	new file:   grange_db.sql.zip

--- grange_ci/application/views/home.php
+++ grange_ci/application/views/home.php
@@ -1,5 +1,9 @@
 <div class="main_content">
-<?php
-	echo 'HOME';
-?>
+
+	<div class="page_title">What is Grange?</div>
+	<div class="page_content">
+		Grange is an organization that works on excellence, leadership and entrepreneurship. All
+		members live by kaizen. Being a part of Grange, a member could continuously improve by and on
+		achieving the three core values.
+  </div><!-- end .page_content -->
 </div><!-- end .main_content -->
